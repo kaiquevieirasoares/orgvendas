2a60b376e31d1de9ed85adb0c70b53704847ac0f
Kaique Vieira <123115955+kaiquevieirasoares@users.noreply.github.com>
2024-06-12 11:57:13 -0300

    Create: Configurando o ambiente de trabalho, adicionando as dependencias
    Configurei o ambiente de trabalho e adicionei as dependencias do Jpa, h2, spring web e Lombok.

---
+++ src/main/java/com/orgvendas/OrgvendasApplication.java
@@ -0,0 +1,13 @@
+package com.orgvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orgvendas/OrgvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orgvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
